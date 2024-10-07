0e523c05251c15fccc4b4733d3d29b5c5dba2d08
Pollyany Carvalho <kerleypollyany@gmail.com>
2024-10-06 21:05:25 -0300

    Criando página Home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,45 @@
 export default function Home() {
- return (
-   <div>
+  return (
+    <Container>
+      <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
+        <input
+          className="w-full border-2 rounded-lg h-9 px-3 outline-none"
+          placeholder="Digite o nome do carro..."
+        />
+        <button className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg">
+          Buscar
+        </button>
+      </section>
 
-   </div>
- );
+      <h1 className="font-bold text-center mt-6 text-2xl mb-4">
+        Carros novos e usados em todo o Brasil
+      </h1>
+
+      <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <section className="w-full bg-white rounded-lg">
+          <img
+            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202305/20230513/bmw-320i-2.0-16v-turbo-flex-m-sport-automatico-wmimagem08152110825.jpg?s=fill&w=552&h=414&q=60"
+            alt="Carro"
+          />
+          <p className="font-bold mt-1 mb-2 px-2">BMW 320i</p>
+
+          <div className="flex flex-col px-2">
+            <span className="text-zinc-700 mb-6">
+              Ano 2016/2016 | 23.000 km
+            </span>
+            <strong className="text-black font-medium text-xl">
+              R$ 190.000
+            </strong>
+          </div>
+
+          <div className="w-full h-px bg-slate-200 my-2"></div>
+
+          <div className="px-2 pb-2">
+            <span className="text-black">Campo Grande - MS</span>
+          </div>
+        </section>
+      </main>
+    </Container>
+  );
 }
