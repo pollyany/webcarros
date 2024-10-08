4d43f582ce8eba337d894a8337080ad8baf6bd87
Pollyany Carvalho <kerleypollyany@gmail.com>
2024-10-08 15:06:32 -0300

    Página cadastro de carros

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
 import { router } from "./App";
+import "./index.css";
+
+import { RouterProvider } from "react-router-dom";
 import AuthProvider from "./contexts/AuthContext";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <AuthProvider>
     <RouterProvider router={router} />
   </AuthProvider>
--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -1,7 +1,27 @@
-export default function New() {
- return (
-   <div>
+import  Container  from "../../../components/container";
+import { DashboardHeader } from "../../../components/panelHeader";
 
-   </div>
- );
+import { FiUpload } from 'react-icons/fi'
+
+export default function New() {
+  return (
+    <Container>
+      <DashboardHeader/>
+
+      <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2">
+        <button className="border-2 w-48 rounded-lg flex items-center justify-center cursor-pointer border-gray-600 h-32 md:w-48">
+          <div className="absolute cursor-pointer">
+            <FiUpload size={30} color="#000" />
+          </div>
+          <div className="cursor-pointer">
+            <input type="file" accept="image/*" className="opacity-0 cursor-pointer" />
+          </div>
+        </button>
+      </div>
+
+      <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2 mt-2">
+        <h1>TESTE</h1>
+      </div>
+    </Container>
+  )
 }
