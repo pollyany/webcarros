c37969c030bea95d4db3401200dd7f381e4b87a0
Pollyany Carvalho <kerleypollyany@gmail.com>
2024-10-07 22:07:34 -0300

    Criando context api

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./App";
+import AuthProvider from "./contexts/AuthContext";
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
 );
