dbee53df54acfbca5408e89d809d8b24af0c7be1
Pollyany Carvalho <kerleypollyany@gmail.com>
2024-10-07 21:27:41 -0300

    Criando página de login

---
+++ src/components/input/index.tsx
@@ -0,0 +1,26 @@
+import { RegisterOptions, UseFormRegister} from 'react-hook-form'
+
+interface InputProps{
+  type: string;
+  placeholder: string;
+  name: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  register: UseFormRegister<any>;
+  error?: string;
+  rules?: RegisterOptions;
+}
+
+export default function Input({ name, placeholder, type, register, rules, error }: InputProps){
+  return(
+    <div>
+      <input
+        className="w-full border-2 rounded-md h-11 px-2"
+        placeholder={placeholder}
+        type={type}
+        {...register(name, rules)}
+        id={name}
+      />
+      {error && <p className="my-1 text-red-500">{error}</p>}
+    </div>
+  )
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,5 @@
+import Container from "../../components/container";
+
 export default function Home() {
   return (
     <Container>
