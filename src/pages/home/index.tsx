2d37964bf3ce3a06cd69523c630a8438deb7d5f2
Pollyany Carvalho <kerleypollyany@gmail.com>
2024-10-08 21:23:01 -0300

    Listando carros cadastrados na plataforma

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,68 @@
-import Container from "../../components/container";
+import { useState, useEffect } from 'react'
+import  Container  from "../../components/container";
+import { Link } from 'react-router-dom'
+import {
+  collection,
+  query,
+  getDocs,
+  orderBy
+} from 'firebase/firestore'
+import { db } from '../../services/firebaseConnection'
 
-export default function Home() {
+interface CarsProps{
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: string | number;
+  city: string;
+  km: string;
+  images: CarImageProps[];
+}
+
+interface CarImageProps{
+  name: string;
+  uid: string;
+  url: string;
+}
+
+
+export function Home() {
+  const [cars, setCars] = useState<CarsProps[]>([])
+
+  useEffect(() => {
+
+    function loadCars(){
+      const carsRef = collection(db, "cars")
+      const queryRef = query(carsRef, orderBy("created", "desc"))
+
+      getDocs(queryRef)
+      .then((snapshot) => {
+        const listcars = [] as CarsProps[];
+
+        snapshot.forEach( doc => {
+          listcars.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            price: doc.data().price,
+            images: doc.data().images,
+            uid: doc.data().uid
+          })
+        })
+
+        setCars(listcars);  
+      })
+
+    }
+
+    loadCars();
+
+  }, [])
+
+
   return (
     <Container>
       <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
@@ -8,7 +70,9 @@ export default function Home() {
           className="w-full border-2 rounded-lg h-9 px-3 outline-none"
           placeholder="Digite o nome do carro..."
         />
-        <button className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg">
+        <button
+          className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg"
+        >
           Buscar
         </button>
       </section>
@@ -17,31 +81,38 @@ export default function Home() {
         Carros novos e usados em todo o Brasil
       </h1>
 
-      <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full bg-white rounded-lg">
-          <img
-            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202305/20230513/bmw-320i-2.0-16v-turbo-flex-m-sport-automatico-wmimagem08152110825.jpg?s=fill&w=552&h=414&q=60"
-            alt="Carro"
-          />
-          <p className="font-bold mt-1 mb-2 px-2">BMW 320i</p>
+      <main className="grid gird-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
 
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700 mb-6">
-              Ano 2016/2016 | 23.000 km
-            </span>
-            <strong className="text-black font-medium text-xl">
-              R$ 190.000
-            </strong>
-          </div>
 
-          <div className="w-full h-px bg-slate-200 my-2"></div>
+      {cars.map( car => (
+        <Link key={car.id} to={`/car/${car.id}`}>
+          <section className="w-full bg-white rounded-lg">
+            <img
+              className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+              src={car.images[0].url}
+              alt="Carro" 
+            />
+            <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
 
-          <div className="px-2 pb-2">
-            <span className="text-black">Campo Grande - MS</span>
-          </div>
-        </section>
+            <div className="flex flex-col px-2">
+              <span className="text-zinc-700 mb-6">Ano {car.year} | {car.km} km</span>
+              <strong className="text-black font-medium text-xl">R$ {car.price}</strong>
+            </div>
+
+            <div className="w-full h-px bg-slate-200 my-2"></div>
+
+            <div className="px-2 pb-2">
+              <span className="text-black">
+                {car.city}
+              </span>
+            </div>
+
+          </section>
+        </Link>
+      ))}
+
+
       </main>
     </Container>
-  );
+  )
 }
